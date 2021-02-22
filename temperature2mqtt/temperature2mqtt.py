c626d5998b15adc05a46aa2347cb192667778652
Jonas Pedersen <jonas@pedersen.ninja>
2021-02-22 20:43:07 +0100

    Ooops...

--- temperature2mqtt/temperature2mqtt.py
+++ temperature2mqtt/temperature2mqtt.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 import time
 import argparse
 import temperature2mqtt.temperatureReader
-from temperature2mqtt.gtemperatureReader import TemperatureProxy
+from temperature2mqtt.temperatureReader import TemperatureProxy
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
